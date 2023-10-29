8b3494eeab12f8e41cda985d536a362149b9ee4f
kezzy <1561081334@qq.com>
2023-10-29 20:49:15 +0800

    完成Lab1_challenge1的系统调用和返回地址打印

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -31,6 +31,16 @@ ssize_t sys_user_exit(uint64 code) {
   shutdown(code);
 }
 
+ssize_t sys_user_print_backtrace(uint64 n){
+    uint64 fp = current->trapframe->regs.s0;
+    fp = *((uint64*)(fp - 8));
+    for(int i = 0;i < n;i++){
+        sprint("%p\n",*(uint64*)(fp - 8));
+        fp = *((uint64*)(fp - 16));
+    }
+    return 0;
+}
+
 //
 // [a0]: the syscall number; [a1] ... [a7]: arguments to the syscalls.
 // returns the code of success, (e.g., 0 means success, fail for otherwise)
@@ -41,6 +51,8 @@ long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, l
       return sys_user_print((const char*)a1, a2);
     case SYS_user_exit:
       return sys_user_exit(a1);
+      case SYS_user_print_backtrace:
+          return sys_user_print_backtrace(a1);
     default:
       panic("Unknown syscall %ld \n", a0);
   }

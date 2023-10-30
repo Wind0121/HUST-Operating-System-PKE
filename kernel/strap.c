f6b07575d7cf4bd18df10fa0aa1dac2f95876d91
kezzy <1561081334@qq.com>
2023-10-30 21:43:38 +0800

    完成Lab2_3

--- kernel/strap.c
+++ kernel/strap.c
@@ -9,6 +9,7 @@
 #include "pmm.h"
 #include "vmm.h"
 #include "util/functions.h"
+#include "memlayout.h"
 
 #include "spike_interface/spike_utils.h"
 
@@ -58,8 +59,9 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
       // dynamically increase application stack.
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
-      panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
-
+      void* pa = alloc_page();
+      if(pa == 0) panic("no more physical pages");
+      user_vm_map(current->pagetable,ROUNDDOWN(stval,PGSIZE),PGSIZE,(uint64)pa,prot_to_type(PROT_WRITE | PROT_READ, 1));
       break;
     default:
       sprint("unknown page fault.\n");

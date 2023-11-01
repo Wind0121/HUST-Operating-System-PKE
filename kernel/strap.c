0c3b8bd6e257214c1929e3d5d503a3810cade214
kezzy <1561081334@qq.com>
2023-11-01 20:46:39 +0800

    完成Lab3_3

--- kernel/strap.c
+++ kernel/strap.c
@@ -75,7 +75,12 @@ void rrsched() {
   // hint: increase the tick_count member of current process by one, if it is bigger than
   // TIME_SLICE_LEN (means it has consumed its time slice), change its status into READY,
   // place it in the rear of ready queue, and finally schedule next process to run.
-  panic( "You need to further implement the timer handling in lab3_3.\n" );
+  current->tick_count++;
+  if(current->tick_count >= TIME_SLICE_LEN){
+      current->tick_count = 0;
+      insert_to_ready_queue(current);
+      schedule();
+  }
 
 }
 
